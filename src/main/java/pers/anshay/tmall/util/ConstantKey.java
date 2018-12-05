28e6eb5f6ebd9e2861f8714097524bea40702365
Anshay <anshaym@163.com>
2018-12-05 23:29:39 +0800

    全局公共常量类

---
+++ src/main/java/pers/anshay/tmall/util/ConstantKey.java
@@ -0,0 +1,15 @@
+package pers.anshay.tmall.util;
+
+/**
+ * 全局公共常量
+ *
+ * @author: Anshay
+ * @date: 2018/12/5
+ */
+public class ConstantKey {
+
+    /**
+     * 导航分页数。类似【1，2，3，4，5】一共显示5个这样
+     */
+    public static final int NAVIGATE_PAGE_SIZE = 5;
+}
