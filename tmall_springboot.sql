834ed73be47204e678bb7bc81fee6bec8b5b3655
Anshay <anshaym@163.com>
2018-12-20 20:53:19 +0800

    sql提交sql文件

---
+++ tmall_springboot.sql
@@ -0,0 +1,207 @@
+﻿# Host: 127.0.0.1  (Version 5.5.15)
+# Date: 2018-12-20 20:51:31
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "category"
+#
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "category"
+#
+
+INSERT INTO `category` VALUES (3,'测试分类3'),(5,'测试分类5s'),(8,'测试分类8'),(9,'测试分类9'),(10,'测试分类10'),(11,'测试1');
+
+#
+# Structure for table "product"
+#
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `subTitle` varchar(255) DEFAULT NULL,
+  `originalPrice` float DEFAULT NULL,
+  `promotePrice` float DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `cid` int(11) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_category` (`cid`),
+  CONSTRAINT `fk_product_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "product"
+#
+
+INSERT INTO `product` VALUES (2,'产品1修改','title1',99.98,19.98,99,3,'2018-12-06 23:34:55'),(3,'产品一','title1',99.98,19.98,99,5,'2018-12-09 23:12:20');
+
+#
+# Structure for table "productimage"
+#
+
+DROP TABLE IF EXISTS `productimage`;
+CREATE TABLE `productimage` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_productimage_product` (`pid`),
+  CONSTRAINT `fk_productimage_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "productimage"
+#
+
+INSERT INTO `productimage` VALUES (6,3,'detail'),(13,3,'detail'),(14,3,'detail'),(15,3,'detail'),(16,3,'single'),(17,2,'detail'),(21,2,'single'),(22,2,'detail'),(23,2,'detail'),(26,2,'single'),(27,2,'detail');
+
+#
+# Structure for table "property"
+#
+
+DROP TABLE IF EXISTS `property`;
+CREATE TABLE `property` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_property_category` (`cid`),
+  CONSTRAINT `fk_property_category` FOREIGN KEY (`cid`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "property"
+#
+
+INSERT INTO `property` VALUES (1,3,'属性1修改'),(3,3,'属性2修改'),(4,3,'属性3');
+
+#
+# Structure for table "propertyvalue"
+#
+
+DROP TABLE IF EXISTS `propertyvalue`;
+CREATE TABLE `propertyvalue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `ptid` int(11) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_propertyvalue_property` (`ptid`),
+  KEY `fk_propertyvalue_product` (`pid`),
+  CONSTRAINT `fk_propertyvalue_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_propertyvalue_property` FOREIGN KEY (`ptid`) REFERENCES `property` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "propertyvalue"
+#
+
+INSERT INTO `propertyvalue` VALUES (1,2,1,NULL),(2,2,3,'属性2的值'),(3,2,4,'属性3的值');
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'测试用户1','password1',NULL),(2,'测试用户2','password2',NULL),(3,'测试用户3','password3',NULL),(4,'测试用户4','password4',NULL),(5,'测试用户5','password5',NULL),(14,'订单用户','password4',NULL);
+
+#
+# Structure for table "review"
+#
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(4000) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_review_product` (`pid`),
+  KEY `fk_review_user` (`uid`),
+  CONSTRAINT `fk_review_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_review_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "review"
+#
+
+
+#
+# Structure for table "order_"
+#
+
+DROP TABLE IF EXISTS `order_`;
+CREATE TABLE `order_` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orderCode` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `post` varchar(255) DEFAULT NULL,
+  `receiver` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `userMessage` varchar(255) DEFAULT NULL,
+  `createDate` datetime DEFAULT NULL,
+  `payDate` datetime DEFAULT NULL,
+  `deliveryDate` datetime DEFAULT NULL,
+  `confirmDate` datetime DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_order_user` (`uid`),
+  CONSTRAINT `fk_order_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "order_"
+#
+
+INSERT INTO `order_` VALUES (1,'201608241638122609867','某某市，某某区，某某街道，某某号 ','610000','某某某','15111111111',NULL,'2016-12-30 00:00:00',NULL,NULL,NULL,14,'waitDelivery');
+
+#
+# Structure for table "orderitem"
+#
+
+DROP TABLE IF EXISTS `orderitem`;
+CREATE TABLE `orderitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `oid` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `number` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_orderitem_user` (`uid`),
+  KEY `fk_orderitem_product` (`pid`),
+  KEY `fk_orderitem_order` (`oid`),
+  CONSTRAINT `fk_orderitem_order` FOREIGN KEY (`oid`) REFERENCES `order_` (`id`),
+  CONSTRAINT `fk_orderitem_product` FOREIGN KEY (`pid`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_orderitem_user` FOREIGN KEY (`uid`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "orderitem"
+#
+
+INSERT INTO `orderitem` VALUES (1,2,1,14,2),(2,3,1,14,2);
