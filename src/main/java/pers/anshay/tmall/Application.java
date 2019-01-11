f9db79a38bc9ed39287f90407200b7482b4a763a
Anshay <anshayM@163.com>
2019-01-11 17:45:48 +0800

    :art:添加打包相关配置

--- src/main/java/pers/anshay/tmall/Application.java
+++ src/main/java/pers/anshay/tmall/Application.java
@@ -2,8 +2,9 @@ package pers.anshay.tmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import pers.anshay.tmall.util.PortUtil;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * 项目启动类
@@ -12,10 +13,16 @@ import pers.anshay.tmall.util.PortUtil;
  * @date: 2018/11/29
  */
 @SpringBootApplication
-@EnableCaching
-public class Application {
-    static {
-        PortUtil.checkPort(6379, "Redis 服务端", true);
+@ServletComponentScan
+//@EnableCaching
+public class Application extends SpringBootServletInitializer {
+    //    static {
+    /*本地调试的时候，用来检测redis服务是否开启，部署时删除*/
+//        PortUtil.checkPort(6379, "Redis 服务端", true);
+//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(Application.class);
     }
 
     public static void main(String[] args) {
